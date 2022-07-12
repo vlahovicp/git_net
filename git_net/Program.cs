f2f99580dafc21440e16bbe873bc471c4912be7c
pavao.vlahovic <pavao.vlahovic@king-ict.hr>
2022-07-12 07:16:30 +0200

    Init project

---
+++ git_net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
